239012e8f93e93eee5e93fbe2194281bf3e7f3a9
DOEMsy <46560164+DOEMsy@users.noreply.github.com>
2020-06-02 17:58:53 +0800

    Update cnblogLoader_cdn.js

--- src/cnblogLoader_cdn.js
+++ src/cnblogLoader_cdn.js
@@ -84,8 +84,8 @@
   };
   
   var __BLOG_CONFIG__ = window.__BLOG_CONFIG__ || {};
-  var staticVer = __BLOG_CONFIG__.staticVer || "";
-  var staticSrc = __BLOG_CONFIG__.staticSrc || "https://cdn.jsdelivr.net/gh/DOEMsy/cnblog_theme_atum";
+  var staticVer = __BLOG_CONFIG__.staticVer;
+  var staticSrc = __BLOG_CONFIG__.staticSrc;
   var staticIco = __BLOG_CONFIG__.staticIco || "https://cjunn.gitee.io/c_cnblog_vue/img/body/miao.ico";
   var extCss = __BLOG_CONFIG__.extCss || [];
   var extJs = __BLOG_CONFIG__.extJs || [];
